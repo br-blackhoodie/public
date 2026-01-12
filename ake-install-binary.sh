7164a16da237c83e1211aa93f52683ca292565c0
BlackHoodie Corporation <noreply@blackhoodie.com>
2026-01-12 17:17:33 -0400

    chore: bump version to 3.0.2
    Changes in this version:
    - Fixed LoadBalancer conditional logic
    - service_lb_cidr only defined when LoadBalancerPool configured
    - Correct inventory path structure for RKE2 playbooks

--- ake-install-binary.sh
+++ ake-install-binary.sh
@@ -28,7 +28,7 @@ cat << 'BANNER'
  / ___ |/ /| |/ /___
 /_/  |_/_/ |_/_____/
 
-AKE Platform Installer v3.0.1
+AKE Platform Installer v3.0.2
 Agnostic Kubernetes Everywhere
 BlackHoodie Corporation
 BANNER
