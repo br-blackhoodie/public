1cafbdde4b6d8e42ce45feb8997e189f625dea69
BlackHoodie Corporation <noreply@blackhoodie.com>
2026-01-12 15:55:33 -0400

    chore: bump version to v3.0.1
    Changes:
    - Fixed git workflow for IaC repository push
    - Updated all version references to 3.0.1

--- ake-install-binary.sh
+++ ake-install-binary.sh
@@ -28,7 +28,7 @@ cat << 'BANNER'
  / ___ |/ /| |/ /___
 /_/  |_/_/ |_/_____/
 
-AKE Platform Installer v3.0.0
+AKE Platform Installer v3.0.1
 Agnostic Kubernetes Everywhere
 BlackHoodie Corporation
 BANNER
