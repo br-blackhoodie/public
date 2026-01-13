496509c8c0d29c5e8654cbbd544567923a9e1996
BlackHoodie Corporation <noreply@blackhoodie.com>
2026-01-13 09:43:59 -0400

    v3.0.6 - Simplify installation flow
    Changes:
    - Removed component selection dialog (GitLab, Vault, Harbor, Monitoring)
    - Removed environments selection dialog
    - All components are now always installed
    - Always creates 3 environments: dev, stg, prod
    - Fixed inconsistent behavior where selections were ignored
    
    This change simplifies the installer by removing non-functional dialogs
    and ensures consistent deployments.

--- ake-install-binary.sh
+++ ake-install-binary.sh
@@ -28,7 +28,7 @@ cat << 'BANNER'
  / ___ |/ /| |/ /___
 /_/  |_/_/ |_/_____/
 
-AKE Platform Installer v3.0.5
+AKE Platform Installer v3.0.6
 Agnostic Kubernetes Everywhere
 BlackHoodie Corporation
 BANNER
